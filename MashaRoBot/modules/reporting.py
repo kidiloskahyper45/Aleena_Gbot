5a282867cf94b98d94b0af9d84bdaf11b758ac0c
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-06-19 11:30:24 +0530

    Update reporting.py

--- MashaRoBot/modules/reporting.py
+++ MashaRoBot/modules/reporting.py
@@ -264,12 +264,12 @@ def buttons(update: Update, context: CallbackContext):
 
 
 __help__ = """
- ❍ /report <reason>*:* reply to a message to report it to admins.
- ❍ @admin*:* reply to a message to report it to admins.
+ 🆘 /report <reason>*:* reply to a message to report it to admins.
+ 🆘 @admin*:* reply to a message to report it to admins.
 *NOTE:* Neither of these will get triggered if used by admins.
 
 *Admins only:*
- ❍ /reports <on/off>*:* change report setting, or view current status.
+ 🆘 /reports <on/off>*:* change report setting, or view current status.
    • If done in pm, toggles your status.
    • If in group, toggles that groups's status.
 """
@@ -285,7 +285,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "REPORTS"
+__mod_name__ = "REPORT"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
