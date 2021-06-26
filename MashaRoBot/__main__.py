fdf880e95b176a5d98cd8c52ee56774922bed914
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-06-26 09:46:13 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -378,20 +378,8 @@ def Source_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "source_":
         query.message.edit_text(
-            text=""" Hi..🤗 I'm *Aleena
-            *This are the T & C Applied on This Bot*
-            - We do not store any kind of Personal 
-              Information.
-            - Cmds/texts Send to bot are 100% Secure.
-            - We do not share any UserData of Group Info.
-            - We Respect your Privacy.
-            - Do not misuse bot for illigal activities.
-            - All rights reserved™️.
-             *Dev*
-            > @tubots
-            -----------
-            Terms and Conditions are Applied©
-          \nHere is the [Privacy and Policy](https://telegram.org/privacy) .""",
+            text=""" Hi..🤗 I'm *Aleena* 
+                 \nHere is the [TERMS AND CONDITIONS](https://telegra.ph/Aleena-06-26) .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=False,
             reply_markup=InlineKeyboardMarkup(
