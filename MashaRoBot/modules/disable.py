45a098532f92d13deb536031761c851a75f7d576
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-07-08 09:52:52 +0530

    Update disable.py

--- MashaRoBot/modules/disable.py
+++ MashaRoBot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
      /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "RESTRICT"
+    __mod_name__ = "Restrict"
 
 else:
     DisableAbleCommandHandler = CommandHandler
