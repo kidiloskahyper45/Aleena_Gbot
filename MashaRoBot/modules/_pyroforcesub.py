05a9a36d387fd49970295026c9f7e1804a14983b
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-07-08 09:38:06 +0530

    Update _pyroforcesub.py

--- MashaRoBot/modules/_pyroforcesub.py
+++ MashaRoBot/modules/_pyroforcesub.py
@@ -198,4 +198,4 @@ __help__ = """
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
   /fsub clear - To unmute all members who muted by me.
 """
-__mod_name__ = "F_ƧЦB"
+__mod_name__ = "F-sub"
